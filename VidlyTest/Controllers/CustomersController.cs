3b4b1bbf7bdba109d87868c273b68ea9256ef805
Tabo Mbaleni <tmbaleni@live.com>
2018-12-05 19:24:55 +0800

    Display Membership Type for customers

--- VidlyTest/Controllers/CustomersController.cs
+++ VidlyTest/Controllers/CustomersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -23,12 +24,13 @@ namespace VidlyTest.Controllers
         public ActionResult Index()
         {
             ViewBag.Message = "Hello from customers controller";
-            var customers = _context.Customers.ToList();
+            //add customers and their relations(//membershipType) from database
+            var customers = _context.Customers.Include( c => c.MembershipType).ToList();
             return View(customers);
         }
         public ActionResult Details(int id)
         {
-            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
             if (customer == null)
                 return HttpNotFound();
             return View(customer);
