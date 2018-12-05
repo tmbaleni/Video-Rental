6891339a13dc94729a5966c0fa5b865f40ca55b2
Tabo Mbaleni <tmbaleni@live.com>
2018-12-05 18:23:16 +0800

    Get Customers From Database

--- VidlyTest/Controllers/CustomersController.cs
+++ VidlyTest/Controllers/CustomersController.cs
@@ -9,29 +9,29 @@ namespace VidlyTest.Controllers
 {
     public class CustomersController : Controller
     {
+        //get Db context
+        private ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Customers
         public ActionResult Index()
         {
             ViewBag.Message = "Hello from customers controller";
-            var customers = GetCustomers();
+            var customers = _context.Customers.ToList();
             return View(customers);
         }
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customer == null)
                 return HttpNotFound();
             return View(customer);
-        }
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "Tabo Mbaleni" },
-                new Customer { Id = 2, Name = "Mary Williams" },
-                new Customer { Id = 3, Name = "Jane Doe" },
-                new Customer { Id = 4, Name = "Serena Williams" }
-            };
         }
     }
 }
