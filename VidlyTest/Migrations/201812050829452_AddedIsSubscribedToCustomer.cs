250ca9632b85fc781a9b505baeaadb742b0d2307
Tabo Mbaleni <tmbaleni@live.com>
2018-12-05 16:44:37 +0800

    Added MembershipType

---
+++ VidlyTest/Migrations/201812050829452_AddedIsSubscribedToCustomer.cs
@@ -0,0 +1,18 @@
+namespace VidlyTest.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedIsSubscribedToCustomer : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "IsSubscribedToNewsLetter", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "IsSubscribedToNewsLetter");
+        }
+    }
+}
--- VidlyTest/Models/Customer.cs
+++ VidlyTest/Models/Customer.cs
@@ -9,5 +9,8 @@ namespace VidlyTest.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
     }
 }
