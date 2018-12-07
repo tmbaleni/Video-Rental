1af971939f497fae5c18d22e08d5a1f69bf251ef
Tabo Mbaleni <tmbaleni@live.com>
2018-12-07 17:14:38 +0800

    Complete Edit Customer Details

--- VidlyTest/Controllers/CustomersController.cs
+++ VidlyTest/Controllers/CustomersController.cs
@@ -42,16 +42,41 @@ namespace VidlyTest.Controllers
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
-            return View(viewModel);
+            return View("CustomerForm", viewModel);
         }
         [HttpPost]
-        public ActionResult Create(Customer customer)
+        public ActionResult Save(Customer customer)
         {
-            _context.Customers.Add(customer);
+            //if customer has id 0 then creat new customer
+            if(customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                //customer has id so fetch details an rewrite fields
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsLetter = customer.IsSubscribedToNewsLetter;
+            }
             _context.SaveChanges();
             return RedirectToAction("Index", "Customers");
+        }
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            return View("CustomerForm", viewModel);
         }
     }
 }
