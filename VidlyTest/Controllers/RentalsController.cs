0e6f13f417f0b4414722b3a8ca20768604fb54ed
Tabo Mbaleni <tmbaleni@live.com>
2018-12-19 11:40:10 +0800

    Get and display Rental info in table

--- VidlyTest/Controllers/RentalsController.cs
+++ VidlyTest/Controllers/RentalsController.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using VidlyTest.Models;
+using VidlyTest.ViewModels;
+using System.Data.Entity;
 
 namespace VidlyTest.Controllers
 {
     public class RentalsController : Controller
     {
+        //get Db context
+        private ApplicationDbContext _context;
+        public RentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Rentals
         public ActionResult Index()
         {
-            return View();
+            var rentals = _context.Rentals.Include(c => c.Movie).Include(c => c.Customer).ToList();
+            return View(rentals);
         }
     }
 }
